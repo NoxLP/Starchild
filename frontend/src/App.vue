b543e3a5ef0d69e87caec26c2628a2501835918d
Norber <nox_nox_@hotmail.com>
2021-03-02 16:45:32 +0000

    Changes to test card and added slot to component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,17 @@
 <template>
   <v-app>
-    <v-main> </v-main>
+    <v-main>
+      <Card>
+        Esto es una prueba
+      </Card>
+    </v-main>
   </v-app>
 </template>
 
-<script></script>
+<script>
+import Card from './components/Card.vue'
+
+export default {
+  components: { Card }
+}
+</script>
--- frontend/src/components/Card.vue
+++ frontend/src/components/Card.vue
@@ -3,7 +3,7 @@
     <v-container>
       <v-row dense>
         <v-col cols="12">
-          <v-card color=""> </v-card>
+          <slot></slot>
         </v-col>
       </v-row>
     </v-container>
