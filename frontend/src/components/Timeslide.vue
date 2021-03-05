54e2a327d121640df8dc4e53fd0008530bf41439
Norber <nox_nox_@hotmail.com>
2021-03-05 10:06:10 +0000

    Timeslide iteration

--- frontend/src/components/Timeslide.vue
+++ frontend/src/components/Timeslide.vue
@@ -7,37 +7,51 @@
       hide-delimiters
       @change="categoriesOnChange"
     >
-      <v-carousel-item class="ml-15">
-        <v-card>
+      <v-carousel-item>
+        <v-card class="text-center" width="150px">
           Categoría 1
         </v-card>
       </v-carousel-item>
-      <v-carousel-item class="ml-15">
-        <v-card>
+      <v-carousel-item>
+        <v-card class="text-center" width="150px">
           Categoría 2
         </v-card>
       </v-carousel-item>
-      <v-carousel-item class="ml-15">
-        <v-card>
+      <v-carousel-item>
+        <v-card class="text-center" width="150px">
           Categoría 3
         </v-card>
       </v-carousel-item>
-      <v-carousel-item class="ml-15">
-        <v-card>
+      <v-carousel-item>
+        <v-card class="text-center" width="150px">
           Categoría 4
         </v-card>
       </v-carousel-item>
     </v-carousel>
-    <v-timeline align-top>
-      <v-timeline-item v-for="(item, idx) in timeItems" :key="idx" fill-dot>
-        <v-card>
+    <v-timeline align-top clipped dense>
+      <v-timeline-item v-for="(item, idx) in timeItems" :key="idx" small>
+        <!--<v-card>
           <v-card-title>
             {{ item.title }}
           </v-card-title>
           <v-card-text>
             {{ item.text }}
           </v-card-text>
-        </v-card>
+        </v-card>-->
+        <template v-slot:opposite>
+          <span
+            :class="`headline font-weight-bold --text`"
+            v-text="item.date"
+          ></span>
+        </template>
+        <div class="py-4">
+          <h2 :class="`headline font-weight-light mb-4 --text`">
+            {{ item.title }}
+          </h2>
+          <div>
+            {{ item.text }}
+          </div>
+        </div>
       </v-timeline-item>
     </v-timeline>
   </div>
@@ -60,7 +74,8 @@ export default {
       for (let i = 0; i < 5; i++) {
         items.push({
           title: `Category ${number + 1} item title ${i}`,
-          text: `Category ${number + 1} item text ${i}`
+          text: `Category ${number + 1} item text ${i}`,
+          date: '04/03/2021'
         })
       }
       return items
