0639e87ce8df682887e974afb657f0ef6e54781b
Norber <nox_nox_@hotmail.com>
2021-03-05 10:23:17 +0000

    Login view created and show it on app.vue for testing

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -7,17 +7,15 @@
 </template>
 
 <script>
-import Button from './components/Button.vue'
 import Footer from './components/Footer.vue'
 import Header from './components/Header.vue'
-import Input from './components/Input.vue'
+import Login from './views/Login'
 
 export default {
   components: {
     Footer,
     Header,
-    Input,
-    Button
+    Login
   }
 }
 </script>
