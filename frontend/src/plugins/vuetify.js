a4903d8cc66eca79a94f79fe87071f84bfd7ded5
RominaElorrieta <rominaelorrieta@gmail.com>
2021-03-05 17:51:32 +0000

    theme colors added for v1.0

--- frontend/src/plugins/vuetify.js
+++ frontend/src/plugins/vuetify.js
@@ -3,4 +3,15 @@ import Vuetify from 'vuetify/lib/framework'
 
 Vue.use(Vuetify)
 
+const vuetify = new Vuetify({
+    theme: {
+        primary: '#180941',
+        secondary: '#3e2f5a',
+        accent: '#e7c296',
+        error: '#ff0000',
+        success: '#00ff66',
+        text: '#ffffff'
+      },
+  })
+
 export default new Vuetify({})
