65ab60b945184099ce6c4b28654cd53f6eeedf66
Norber <nox_nox_@hotmail.com>
2021-03-06 18:25:55 +0000

    Update

--- frontend/src/components/Card.vue
+++ frontend/src/components/Card.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-card max-width="400" class="mx-auto">
+  <v-card :maxWidth="maxWidth" class="mx-auto starchild-card">
     <v-container>
       <v-row dense>
         <v-col cols="12">
@@ -14,8 +14,24 @@
 export default {
   data: () => ({
     items: []
-  })
+  }),
+  props: {
+    maxWidth: Number
+  }
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.starchild-card {
+  background-image: linear-gradient(
+    180deg,
+    rgba(255, 255, 255, 0.1) 0%,
+    rgba(255, 255, 255, 0) 100%
+  ) !important;
+  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+  backdrop-filter: blur(10px);
+  /* Note: backdrop-filter has minimal browser support */
+
+  border-radius: 20px;
+}
+</style>
