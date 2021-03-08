66d089f4d717860342061b07d2c5ce7efaa3a3c8
Norber <nox_nox_@hotmail.com>
2021-03-08 11:53:19 +0000

    Categories arry const to new file

--- frontend/src/components/Timeslide.vue
+++ frontend/src/components/Timeslide.vue
@@ -40,47 +40,15 @@
 </template>
 
 <script>
+/*
+timeItems: { title, date, img, highlight, category }
+*/
+import Categories from './helpers/categories.js'
+
 export default {
   data: () => ({
     model: null,
-    categories: [
-      {
-        name: 'all',
-        text: 'Todas',
-        img: require('../../public/assets/images/categories/imgCatAll.jpg')
-      },
-      {
-        name: 'eclipse_moon',
-        text: 'Eclipses lunares',
-        img: require('../../public/assets/images/categories/imgCatEclipseMoon.png')
-      },
-      {
-        name: 'eclipse_sun',
-        text: 'Eclipses solares',
-        img: require('../../public/assets/images/categories/imgCatEclipseSun.png')
-      },
-      {
-        name: 'planets',
-        text: 'Planetas',
-        img: require('../../public/assets/images/categories/imgCatPlanets.png')
-      },
-      {
-        name: 'meteor_shower',
-        text: 'Lluvia de estrellas',
-        img: require('../../public/assets/images/categories/imgCatMeteorShower.png')
-      },
-      {
-        name: 'comets',
-        text: 'Cometas',
-        img: require('../../public/assets/images/categories/imgCatComets.png'),
-        icon: require('../../public/assets/images/12-astronomy-and-space icons/SVG/8.svg')
-      },
-      {
-        name: 'conjunction',
-        text: 'Alineación',
-        img: require('../../public/assets/images/categories/imgCatConjunction.png')
-      }
-    ],
+    categories: Categories,
     timeItems: [
       {
         title: '',
---
+++ frontend/src/components/helpers/categories.js
@@ -0,0 +1,40 @@
+const CATEGORIES = [
+  {
+    name: 'all',
+    text: 'Todas',
+    img: require('@/../public/assets/images/categories/imgCatAll.jpg')
+  },
+  {
+    name: 'eclipse_moon',
+    text: 'Eclipses lunares',
+    img: require('@/../public/assets/images/categories/imgCatEclipseMoon.png')
+  },
+  {
+    name: 'eclipse_sun',
+    text: 'Eclipses solares',
+    img: require('@/../public/assets/images/categories/imgCatEclipseSun.png')
+  },
+  {
+    name: 'planets',
+    text: 'Planetas',
+    img: require('@/../public/assets/images/categories/imgCatPlanets.png')
+  },
+  {
+    name: 'meteor_shower',
+    text: 'Lluvia de estrellas',
+    img: require('@/../public/assets/images/categories/imgCatMeteorShower.png')
+  },
+  {
+    name: 'comets',
+    text: 'Cometas',
+    img: require('@/../public/assets/images/categories/imgCatComets.png'),
+    icon: require('@/../public/assets/images/12-astronomy-and-space icons/SVG/8.svg')
+  },
+  {
+    name: 'conjunction',
+    text: 'Alineación',
+    img: require('@/../public/assets/images/categories/imgCatConjunction.png')
+  }
+]
+
+export default { CATEGORIES }
