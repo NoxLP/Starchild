75d505b511aac999513d57442b498307998677c7
Jonatan <jonatansg@hotmail.com>
2021-03-09 10:37:36 +0000

    Created comments branch and Comments.vue

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,28 +1,34 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 //import Home from '../views/Home.vue'
-import Signup from '../views/Signup.vue'
-import Login from '../views/Login.vue'
+// import Signup from '../views/Signup.vue'
+// import Login from '../views/Login.vue'
+import Comments from '../views/Comments.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'login',
-    component: Login
-    /*name: 'home',
+  //{
+  // path: '/',
+  // name: 'login',
+  // component: Login
+  /*name: 'home',
     component: Home*/
-  },
+  //},
+  // {
+  //   path: '/signup',
+  //   name: 'signup',
+  //   component: Signup
+  // },
+  // {
+  //   path: '/login',
+  //   name: 'login',
+  //   component: Login
+  // },
   {
-    path: '/signup',
-    name: 'signup',
-    component: Signup
-  },
-  {
-    path: '/login',
-    name: 'login',
-    component: Login
+    path: '/comments',
+    name: 'comments',
+    component: Comments
   }
 ]
 
---
+++ frontend/src/views/Comments.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <Card
+      ><v-img :src="img"></v-img>
+      <p>{{ title }}</p></Card
+    >
+    <Card>
+      <v-container>
+        <v-row>
+          <v-col>
+            <div>
+              <v-img :src="cat_icon" :width="icon_width"></v-img>
+            </div>
+            <div>
+              <v-img :src="moonphase"></v-img>
+              <v-img :src="weather_icon"></v-img>
+            </div>
+          </v-col>
+        </v-row>
+      </v-container>
+    </Card>
+    <Card />
+  </div>
+</template>
+
+<script>
+import Card from '../components/Card.vue'
+export default {
+  data: function() {
+    return {
+      img: '',
+      title: '',
+      icon_width: 30,
+      cat_icon: '',
+      moonphase: '',
+      weather_icon: ''
+    }
+  },
+  components: { Card }
+}
+</script>
+
+<style scoped></style>
