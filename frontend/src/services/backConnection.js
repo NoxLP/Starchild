6f0eb119e6ccdc2f2577758c6c7ccedb0ef813f1
RominaElorrieta <rominaelorrieta@gmail.com>
2021-03-13 12:46:26 +0000

    done

--- frontend/src/services/backConnection.js
+++ frontend/src/services/backConnection.js
@@ -1,8 +1,7 @@
 const axios = require('axios')
-require('dotenv').config()
 
 const api = axios.create({
-  baseURL: process.env.BACK_URL,
+  baseURL: "http://localhost:8010/api",
   withCredentials: false
 })
 
