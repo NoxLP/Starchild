a7b043b907810ad3ea61396b9d595245debdc8e5
Jonatan <jonatansg@hotmail.com>
2021-03-13 10:45:44 +0000

    function getEvent and getEventMoonPhase created

--- frontend/src/services/eventServices.js
+++ frontend/src/services/eventServices.js
@@ -1,6 +1,50 @@
 import { api } from './backConnection.js'
 
 export default {
+  // /events/last
+  getLastEvents: async function() {
+    let events
+    try {
+      events = (await api.get('/events/last')).data
+    } catch (err) {
+      console.log(err)
+    }
+    return events
+  },
+  // /events/timelinedtos/:categoryId
+  getTimelineDTOs: async function(category, limit) {
+    let dtos
+    try {
+      dtos = (
+        await api.get(`/events/timelinedtos/${category}`, {
+          params: { limit: limit }
+        })
+      ).data
+    } catch (err) {
+      console.log(err)
+    }
+    return dtos
+  },
+  //events/:eventId
+  getEvent: async function(eventId) {
+    let event
+    try {
+      event = (await api.get(`/events/${eventId}`)).data
+    } catch (err) {
+      console.log(err)
+    }
+    return event
+  },
+  //events/:eventId/moon
+  getEventMoonPhase: async function(eventId) {
+    let event
+    try {
+      event = (await api.get(`/events/${eventId}/moon`)).data
+    } catch (err) {
+      console.log(err)
+    }
+    return event
+  },
   getEventImage: async function(eventId) {
     let image
     try {
