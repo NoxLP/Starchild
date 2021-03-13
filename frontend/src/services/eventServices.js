8261ab2ee16893919dbcdb0e92bdc1279fd79f0e
Norber <nox_nox_@hotmail.com>
2021-03-13 10:48:25 +0000

    Fixed home services in event services file

--- frontend/src/services/eventServices.js
+++ frontend/src/services/eventServices.js
@@ -1,30 +1,6 @@
 import { api } from './backConnection.js'
 
 export default {
-  // /events/last
-  getLastEvents: async function() {
-    let events
-    try {
-      events = (await api.get('/events/last')).data
-    } catch (err) {
-      console.log(err)
-    }
-    return events
-  },
-  // /events/timelinedtos/:categoryId
-  getTimelineDTOs: async function(category, limit) {
-    let dtos
-    try {
-      dtos = (
-        await api.get(`/events/timelinedtos/${category}`, {
-          params: { limit: limit }
-        })
-      ).data
-    } catch (err) {
-      console.log(err)
-    }
-    return dtos
-  },
   //events/:eventId
   getEvent: async function(eventId) {
     let event
