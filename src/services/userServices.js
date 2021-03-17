d2cae42936a7f65ee975d24d7350a779b3a60727
Norber <nox_nox_@hotmail.com>
2021-03-17 19:29:05 +0000

    fixes

--- src/services/userServices.js
+++ src/services/userServices.js
@@ -6,13 +6,34 @@ export default {
     let favourite
     try {
       favourite = (
-        await api.put(`/users/events/favourites/${eventId}`, {
-          headers: { token: localStorage.getItem('token') }
-        })
+        await api.put(
+          `/users/events/favourites/${eventId}`,
+          {},
+          {
+            headers: { token: localStorage.getItem('token') }
+          }
+        )
       ).data
     } catch (err) {
       console.log(err)
     }
     return favourite
+  },
+  getUser: async function() {
+    console.log('get user')
+
+    let user
+    try {
+      user = (
+        await api.get('/users/me', {
+          headers: { token: localStorage.getItem('token') }
+        })
+      ).data
+      console.log('USER: ', user)
+    } catch (err) {
+      console.log('error retrieving user data: ', err)
+    }
+
+    return user
   }
 }
