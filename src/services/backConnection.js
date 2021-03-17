808efde6bbd9bd6a635e09d7396431b96e474d88
Norber <nox_nox_@hotmail.com>
2021-03-17 08:10:09 +0000

    Removed proxy port

--- src/services/backConnection.js
+++ src/services/backConnection.js
@@ -1,7 +1,7 @@
 const axios = require('axios')
 
 const api = axios.create({
-  baseURL: 'https://starback.herokuapp.com:443',
+  baseURL: 'https://starback.herokuapp.com',
   withCredentials: false
 })
 
