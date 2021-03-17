2c02500a4d6a9d62af2a22acc307ba8bb2024baf
Norber <nox_nox_@hotmail.com>
2021-03-17 20:04:04 +0000

    Fixed two user services

--- src/services/userServices.js
+++ src/services/userServices.js
@@ -14,5 +14,22 @@ export default {
       console.log(err)
     }
     return favourite
+  },
+  getUser: async function() {
+    console.log('get user')
+
+    let user
+    try {
+      user = (
+        await api.get('/users/me', {
+          headers: { token: localStorage.getItem('token') }
+        })
+      ).data
+      console.log('USER: ', user)
+    } catch (err) {
+      console.log('error retrieving user data: ', err)
+    }
+
+    return user
   }
 }
